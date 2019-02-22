f760993e3f3e4dc6c58f37e4e77309a57f8324f7
hyji1 <hyji1@DESKTOP-239DPIM>
2019-02-22 16:50:01 +0900

    no message

--- js/ui_jquery.js
+++ js/ui_jquery.js
@@ -63,6 +63,8 @@ $(document).ready(function(){
 				autoHeight: true,
 				initialSlide:initSlide,
 				noSwipingClass: 'stop-swiping',
+				preventClicks : false,
+				preventClicksPropagation : false,
 				thumbs: {
 					swiper: swipeMenu
 				},
