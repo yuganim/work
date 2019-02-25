32194040ed29b1df2a6f9996b2b001230012245b
hyji1 <hyji1@DESKTOP-239DPIM>
2019-02-25 11:03:02 +0900

    no message

--- js/ui_jquery.js
+++ js/ui_jquery.js
@@ -101,6 +101,31 @@ $(document).ready(function(){
 				}
 			}
 		});
+
+		var cntHig = new Array();
+		$('.tab_cnt.swiper-slide').each(function(i){
+			//cntHig[i] = $('.tab_cnt.swiper-slide').eq(i).offsetParent().height();
+			cntHig[i] = $('.tab_cnt.swiper-slide').eq(i).outerHeight();
+		});
+			alert(cntHig[4])
+
+		$('.accordion dt a').on('click', function(e){
+
+			//alert(clickHig)
+			var $this = $(e.target).parent('dt');
+			$this.addClass('on');
+			$this.next('dd').addClass('on').css({'display': 'block'});
+			$this.siblings('dt').removeClass('on');
+			$this.next('dd').siblings('dd').removeClass('on').css({'display': 'none'});
+
+			var ddHig = $this.next('dd').height();
+			var idx = $(e.target).closest('.tab_cnt').index();
+			alert('ddHig / '+ddHig);
+			alert('cntHig[idx] / '+cntHig[idx]);
+			alert('ddHig+cntHig[idx] / '+(ddHig+cntHig[idx]));
+			$('.content').height(ddHig + cntHig[idx]);
+
+		});	
 	}
 
 	$(window).scroll(function(){
@@ -118,30 +143,4 @@ $(document).ready(function(){
 		var h = $(window).outerHeight();
 		$('#wrap_idx').css({'height' : h, 'width' : w});
 	}).resize();
-
-
-
-
-	$('.accordion dt a').on('click', function(e){
-	var cntHig = new Array();
-	$('.tab_cnt.swiper-slide').each(function(i){
-		//cntHig[i] = $('.tab_cnt.swiper-slide').eq(i).offsetParent().height();
-		cntHig[i] = $('.tab_cnt.swiper-slide').eq(i).outerHeight();
-		alert(cntHig[i])
-	});
-		//alert(clickHig)
-		var $this = $(e.target).parent('dt');
-		$this.addClass('on');
-		$this.next('dd').addClass('on').css({'display': 'block'});
-		$this.siblings('dt').removeClass('on');
-		$this.next('dd').siblings('dd').removeClass('on').css({'display': 'none'});
-
-		var ddHig = $this.next('dd').height();
-		var idx = $(e.target).closest('.tab_cnt').index();
-		alert('ddHig / '+ddHig);
-		alert('cntHig[idx] / '+cntHig[idx]);
-		alert('ddHig+cntHig[idx] / '+(ddHig+cntHig[idx]));
-		$('.content').height(ddHig + cntHig[idx]);
-
-	});	
 });
